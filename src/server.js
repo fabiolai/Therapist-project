96f3ab6efd14b3c962ff1e6cfd75d590c54fca95
fabiolai <fabykalifa@gmail.com>
2022-08-18 07:53:40 -0230

    nodemailer

--- src/server.js
+++ src/server.js
@@ -3,10 +3,41 @@ import "dotenv/config";
 import bodyParser from "body-parser";
 import route from "./routes/index";
 import mongoose from "mongoose";
+import { reset } from "nodemon";
+import node from "@babel/register/lib/node";
 
+
+const nodemailer = require("nodemailer");
 const app = express();
+app.use(bodyParser.urlencoded({extended:false}));
 
 app.use(bodyParser.json());
+app.get('/',(req,res) => {
+  reset.send('hello fio!')
+})
+app.post('/',async(req,res) =>{
+  const {email} = req.body;
+
+  let transporter = nodemailer.createTransport({
+    host:"smtp.gmail.com",
+    port:587,
+    secure:false,
+    auth:{
+      user:'username@gmail.com', 
+      pass: 'app password',
+    }
+  });
+  const msg ={
+    from:'"the Express App" <fabykalifa@gmail.com>',
+    to: "${delicefion@gmail.com}",
+    subject:"hello",
+    text:"hey you"
+  }
+  const info = await transporter.sendMail(msg);
+  console.log("message sent:%s", info.messageId);
+  console.log("preview URL:%s",nodemailer.getTestMessageUrl(info));
+  res.send("email.sent")
+})
 app.use("/", route);
 const database = process.env.DATABASE;
 mongoose
